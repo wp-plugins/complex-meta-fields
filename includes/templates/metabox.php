9ce2e0593d93ec3383bef5d928520eed62af2495
Anton Korotkoff <Anton Korotkoff@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-06 23:06:07 +0000

    1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/complex-meta-fields/tags/1.0.1@1039637 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/metabox.php
@@ -0,0 +1,39 @@
+<?php 
+
+/**
+ * Metabox UI
+ */
+
+ $_  = $return;
+ $__ = $metabox['args'];
+ 
+?>
+
+<div class="metabox" ng-controller="cmfMetaBox">
+  <ul ng-init='initialize(<?php echo json_encode( !empty( $_ ) ? $_ : array() ) ?>, <?php echo json_encode( !empty( $__ ) ? $__ : array() ) ?>)'>
+    
+    <li class="fieldset" ng-repeat="fieldset in fieldsets">
+
+      <!-- The list of fields for this metabox -->
+      <ul>
+
+        <!-- Field Item. Loads input templates -->
+        <li class="field" ng-repeat="field in fieldset.options" ng-include="templates_url + 'form/' + field.input + '.php'"></li>
+
+      </ul>
+      
+      <!-- Delete button -->
+      <div class="button button-primary" ng-click="removeFieldSet(fieldsets, $index)"><?php _e( 'Delete', WP_CMF_DOMAIN ); ?></div>
+
+    </li>
+    
+
+  </ul>
+
+  <!-- Add New button -->
+  <div class="button button-primary right" ng-click="addFieldSet(fieldsets)"><?php _e( 'Add New', WP_CMF_DOMAIN ); ?></div>
+  
+  <div class="clear"></div>
+  
+</div>
+
